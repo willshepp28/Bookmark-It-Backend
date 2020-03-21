9ca122a6b8ebae17a3511a4d2f66ebbeb42117ee
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-21 11:39:02 -0400

    included a /topics/creatNewTopic endpoint to createNewTopics

--- api/topics.api.js
+++ api/topics.api.js
@@ -4,12 +4,26 @@ const models = require("../db/models");
 
 
 
-
+// Get all topics
 router.get("/", (request, response) => {
     models.Topic.findAll().then(topics => {
         return response.json(topics);
     });
 });
+
+
+
+
+// Create new topic
+router.post("/createNewTopic", (request, response) => {
+    const body = request.body;
+
+    models.Topic.create(body).then((user) => {
+        return response.json(user)
+    }).catch((error) => {
+        return response.json(error);
+    })
+})
 
 
 
